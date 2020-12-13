6c651430b95a0cf1434490315cd43a435c9ecace
Nathan Cloud <nathan.cloud.10@gmail.com>
2020-12-12 20:37:29 -0800

    Initial Working Example

--- DateFixTool.py
+++ DateFixTool.py
@@ -2,19 +2,35 @@ from dateparser.search import search_dates
 from pandas import ExcelWriter, read_excel
 import xlrd
 import openpyxl
-
-badDate = "bought on September 9th 20"
+import datetime
+import dateutil.parser as dparser
 
 def ExcelHandler(FileName):
-    ExcelInput = read_excel(FileName)
+    ExcelInput = read_excel(FileName, converters={'Date':dateFix})
     with ExcelWriter('Fixed_File.xlsx', engine='xlsxwriter') as writer:
         ExcelInput.to_excel(writer)
+        
 
-def dateFix(bdate): 
-    matches = search_dates(bdate) 
-    firstMatch =  matches[0]
-    goodDate = firstMatch[1]
-    print(goodDate) 
+def dateFix(bdate):
+    if type(bdate) is datetime.datetime:
+        return bdate 
+    matches = dparser.parse(bdate,fuzzy=True)
+    return matches
 
-dateFix(badDate)
+    # matches = re.findall(r"[0-9]{1,5}\/[0-9]{2}\/[0-9]{2,5}", bdate)
+    # if matches:
+    #     result = datetime.datetime.strptime(matches[0],'%x')
+    #     return result
+
+    # matches = search_dates(bdate)
+    # print(matches)
+    # firstMatch =  matches[0]
+    # goodDate = firstMatch[1]
+    # return goodDate.strftime('%Y-%m-%d')
+
 ExcelHandler('BadDates.xlsx')
+# This will return something
+#   re.findall(r"[0-9]{1,5}\/[0-9]{2}\/[0-9]{2,5}", "bought 10/23/96")
+# That you can pass to this
+#   datetime.datetime.strptime('10/23/96', '%m/%d/%y')
+# to get a Date Object and then return that
